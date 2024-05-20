4363a621b0a515900ecb07523769237b4ec61408
John Roice Aldeza <jroicealdeza@gmail.com>
2024-05-20 15:09:40 +0800

    feat: Add employee table migration with columns for personal information, address, contact information, and employee role

--- database/migrations/2024_05_20_065010_create_employee_table.php
+++ database/migrations/2024_05_20_065010_create_employee_table.php
@@ -14,6 +14,24 @@ return new class extends Migration
         Schema::create('employee', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string("first_name", length: 255);
+            $table->string("middle_name");
+            $table->string("last_name", length: 255);
+
+            //columns for address, in the philippines
+            $table->string("street_address");
+            $table->string("barangay");
+            $table->string("city");
+            $table->string("province");
+            $table->string("zip_code");
+            $table->string("country");
+
+            //columns for contact information
+            $table->string("phone_number");
+            $table->string("email_address");
+
+            //columns for employee information
+            $table->string("employee_role");
         });
     }
 
