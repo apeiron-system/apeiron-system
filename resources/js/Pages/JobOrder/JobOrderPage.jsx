e5ef967a77453a2d9944f42b48a8675d6e132ceb
John Roice Aldeza <jroicealdeza@gmail.com>
2024-05-21 14:27:32 +0800

    refactor: Update AuthenticatedLayout component to use "Under Construction!" message

--- resources/js/Pages/JobOrder/JobOrderPage.jsx
+++ resources/js/Pages/JobOrder/JobOrderPage.jsx
@@ -17,7 +17,7 @@ export default function JobOrderPage({ auth }) {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
-                            You're logged in!
+                            Under Construction!
                         </div>
                     </div>
                 </div>
