d099b850525c5cd62ba9e3af8e85352539bbde0c
gpbacay <giannebacay2004@gmail.com>
2024-05-28 09:11:51 +0800

    updated SaveJobOrderModal.jsx

--- resources/js/Components/SaveJobOrderModal.jsx
+++ resources/js/Components/SaveJobOrderModal.jsx
@@ -5,27 +5,30 @@ const Modal = ({ show, onClose }) => {
 
     return (
         <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center">
-            <div className="bg-white p-6 rounded shadow-lg text-center">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-12 w-12 text-green-500 mx-auto"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                >
-                    <path
+            <div className="bg-white p-6 rounded-2xl shadow-lg text-center" style={{ width: '300px', height: '300px' }}>
+                <div className="flex justify-center">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="120"
+                        height="120"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
                         strokeLinecap="round"
                         strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M5 13l4 4L19 7"
-                    />
-                </svg>
+                        className="lucide lucide-check-check"
+                    >
+                        <path d="M18 6 7 17l-5-5" />
+                        <path d="m22 10-7.5 7.5L13 16" />
+                    </svg>
+                </div>
                 <p className="text-lg mt-4">
                     Form has been successfully saved!
                 </p>
                 <button
                     onClick={onClose}
-                    className="mt-6 bg-blue-500 text-white py-2 px-4 rounded"
+                    className="mt-6 bg-[rgb(15,23,42)] hover:bg-[rgb(47,60,78)] text-white py-2 px-4 rounded"
                 >
                     Done
                 </button>
