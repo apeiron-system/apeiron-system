7856fa192805aa2d345963a084da79b6f32088c9
xnea <120112574+xmnhvn@users.noreply.github.com>
2024-11-20 15:15:41 +0800

    for testing

--- database/migrations/2024_05_28_165011_modify_contract_id_foreign_key_on_project.php
+++ database/migrations/2024_05_28_165011_modify_contract_id_foreign_key_on_project.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('project', function (Blueprint $table) {
             // Drop the existing foreign key constraint
-            $table->dropForeign(['contract_id']);
+            // $table->dropForeign(['contract_id']);
 
             // Add the new foreign key constraint with onDelete('cascade')
             $table->foreign('contract_id')
@@ -30,7 +30,7 @@ return new class extends Migration
     {
         Schema::table('project', function (Blueprint $table) {
             // Drop the foreign key with cascade delete
-            $table->dropForeign(['contract_id']);
+            // $table->dropForeign(['contract_id']);
 
             // Revert back to the original foreign key without cascade delete
             $table->foreign('contract_id')
