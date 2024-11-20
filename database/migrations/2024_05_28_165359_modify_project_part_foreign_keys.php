3004106d54ea2ac79b0690efc84a556f16c0983c
Alexander Villasis <alexandervaugn@gmail.com>
2024-11-20 08:14:11 +0800

    Refactor foreign key constraints in project and project_part tables

--- database/migrations/2024_05_28_165359_modify_project_part_foreign_keys.php
+++ database/migrations/2024_05_28_165359_modify_project_part_foreign_keys.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::table('project_part', function (Blueprint $table) {
             // Drop the existing foreign key constraints
-            // $table->dropForeign(['project_id']);
-            // $table->dropForeign(['parent_id']);
+            $table->dropForeign(['project_id']);
+            $table->dropForeign(['parent_id']);
 
             // Add the new foreign key constraints with onDelete('cascade')
             $table->foreign('project_id')
@@ -34,19 +34,19 @@ return new class extends Migration
      */
     public function down(): void
     {
-        // Schema::table('project_part', function (Blueprint $table) {
-        //     // Drop the foreign keys with cascade delete
-        //     $table->dropForeign(['project_id']);
-        //     $table->dropForeign(['parent_id']);
+        Schema::table('project_part', function (Blueprint $table) {
+            // Drop the foreign keys with cascade delete
+            $table->dropForeign(['project_id']);
+            $table->dropForeign(['parent_id']);
 
-        //     // Revert back to the original foreign keys without cascade delete
-        //     $table->foreign('project_id')
-        //           ->references('id')
-        //           ->on('project');
+            // Revert back to the original foreign keys without cascade delete
+            $table->foreign('project_id')
+                  ->references('id')
+                  ->on('project');
 
-        //     $table->foreign('parent_id')
-        //           ->references('id')
-        //           ->on('project_part');
-        // });
+            $table->foreign('parent_id')
+                  ->references('id')
+                  ->on('project_part');
+        });
     }
 };
