a6d2f99d20215c4a74592fd4a08ddf8cba0212c9
John Roice Aldeza <jroicealdeza@gmail.com>
2024-11-21 12:59:36 +0800

    uncomment columns

--- database/migrations/2024_05_28_165359_modify_project_part_foreign_keys.php
+++ database/migrations/2024_05_28_165359_modify_project_part_foreign_keys.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::table('project_part', function (Blueprint $table) {
             // Drop the existing foreign key constraints
-            // $table->dropForeign(['project_id']);
-            // $table->dropForeign(['parent_id']);
+            $table->dropForeign(['project_id']);
+            $table->dropForeign(['parent_id']);
 
             // Add the new foreign key constraints with onDelete('cascade')
             $table->foreign('project_id')
@@ -36,8 +36,8 @@ return new class extends Migration
     {
         Schema::table('project_part', function (Blueprint $table) {
             // Drop the foreign keys with cascade delete
-            // $table->dropForeign(['project_id']);
-            // $table->dropForeign(['parent_id']);
+            $table->dropForeign(['project_id']);
+            $table->dropForeign(['parent_id']);
 
             // Revert back to the original foreign keys without cascade delete
             $table->foreign('project_id')
